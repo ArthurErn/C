6389cd0b089bcf06bc43aa565520fb299c7a5792
sys-arthur <70970354+ArthurErn@users.noreply.github.com>
2023-04-19 17:14:50 -0300

    Update TrianguloDeFloyd.c

--- TrianguloDeFloyd.c
+++ TrianguloDeFloyd.c
@@ -7,9 +7,9 @@ int main()
     printf("Escreva um numero ");
     scanf("%d", &num);
     
-        for(int i = 0; i <= (num + 1); i++){
+        for(int i = 0; i < num; i++){
             printf("\n");
-            for(int j = 1; j <= (num + 1); j++){
+            for(int j = 1; j < num; j++){
                 if(i > j){
                     aux = aux + 1;
                     printf("%d ", aux);
